baa0bb3c7c5fefdac2aca390ccee0ff53fd8c23f
HaiMing.Yin <HaiMing.Yin@gmail.com>
2011-09-19 00:08:31 +0800

    httpclient for app engine

--- tornado/httpclient.py
+++ tornado/httpclient.py
@@ -1,198 +1,79 @@
-"""Blocking and non-blocking HTTP client interfaces.
+"""HTTP client for Google App Engine.
 
-This module defines a common interface shared by two implementations,
-`simple_httpclient` and `curl_httpclient`.  Applications may either
-instantiate their chosen implementation class directly or use the
-`AsyncHTTPClient` class from this module, which selects an implementation
-that can be overridden with the `AsyncHTTPClient.configure` method.
-
-The default implementation is `simple_httpclient`, and this is expected
-to be suitable for most users' needs.  However, some applications may wish
-to switch to `curl_httpclient` for reasons such as the following:
-
-* `curl_httpclient` is more likely to be compatible with sites that are
-  not-quite-compliant with the HTTP spec, or sites that use little-exercised
-  features of HTTP.
-
-* `simple_httpclient` only supports SSL on Python 2.6 and above.
-
-* `curl_httpclient` is faster
-
-* `curl_httpclient` was the default prior to Tornado 2.0.
-
-Note that if you are using `curl_httpclient`, it is highly recommended that
-you use a recent version of ``libcurl`` and ``pycurl``.  Currently the minimum
-supported version is 7.18.2, and the recommended version is 7.21.1 or newer.
+Support most tornado httpclient interface.
 """
-
 import calendar
-import email.utils
+import email
+import functools
 import httplib
+import logging
 import time
-import weakref
 
-from tornado.escape import utf8
 from tornado import httputil
-from tornado.ioloop import IOLoop
-from tornado.util import import_object, bytes_type
+from tornado.escape import utf8
 
-class HTTPClient(object):
-    """A blocking HTTP client.
+from google.appengine.api import urlfetch
 
-    This interface is provided for convenience and testing; most applications
-    that are running an IOLoop will want to use `AsyncHTTPClient` instead.
-    Typical usage looks like this::
-
-        http_client = httpclient.HTTPClient()
-        try:
-            response = http_client.fetch("http://www.google.com/")
-            print response.body
-        except httpclient.HTTPError, e:
-            print "Error:", e
-    """
-    def __init__(self):
-        self._io_loop = IOLoop()
-        self._async_client = AsyncHTTPClient(self._io_loop)
-        self._response = None
-        self._closed = False
-
-    def __del__(self):
-        self.close()
-
-    def close(self):
-        """Closes the HTTPClient, freeing any resources used."""
-        if not self._closed:
-            self._async_client.close()
-            self._io_loop.close()
-            self._closed = True
-
-    def fetch(self, request, **kwargs):
-        """Executes a request, returning an `HTTPResponse`.
-        
-        The request may be either a string URL or an `HTTPRequest` object.
-        If it is a string, we construct an `HTTPRequest` using any additional
-        kwargs: ``HTTPRequest(request, **kwargs)``
-
-        If an error occurs during the fetch, we raise an `HTTPError`.
-        """
-        def callback(response):
-            self._response = response
-            self._io_loop.stop()
-        self._async_client.fetch(request, callback, **kwargs)
-        self._io_loop.start()
-        response = self._response
-        self._response = None
-        response.rethrow()
-        return response
 
 class AsyncHTTPClient(object):
-    """An non-blocking HTTP client.
-
-    Example usage::
-
-        import ioloop
-
-        def handle_request(response):
-            if response.error:
-                print "Error:", response.error
-            else:
-                print response.body
-            ioloop.IOLoop.instance().stop()
-
-        http_client = httpclient.AsyncHTTPClient()
-        http_client.fetch("http://www.google.com/", handle_request)
-        ioloop.IOLoop.instance().start()
-
-    The constructor for this class is magic in several respects:  It actually
-    creates an instance of an implementation-specific subclass, and instances
-    are reused as a kind of pseudo-singleton (one per IOLoop).  The keyword
-    argument force_instance=True can be used to suppress this singleton
-    behavior.  Constructor arguments other than io_loop and force_instance
-    are deprecated.  The implementation subclass as well as arguments to
-    its constructor can be set with the static method configure()
-    """
-    _impl_class = None
-    _impl_kwargs = None
-
-    @classmethod
-    def _async_clients(cls):
-        assert cls is not AsyncHTTPClient, "should only be called on subclasses"
-        if not hasattr(cls, '_async_client_dict'):
-            cls._async_client_dict = weakref.WeakKeyDictionary()
-        return cls._async_client_dict
-
-    def __new__(cls, io_loop=None, max_clients=10, force_instance=False, 
-                **kwargs):
-        io_loop = io_loop or IOLoop.instance()
-        if cls is AsyncHTTPClient:
-            if cls._impl_class is None:
-                from tornado.simple_httpclient import SimpleAsyncHTTPClient
-                AsyncHTTPClient._impl_class = SimpleAsyncHTTPClient
-            impl = AsyncHTTPClient._impl_class
-        else:
-            impl = cls
-        if io_loop in impl._async_clients() and not force_instance:
-            return impl._async_clients()[io_loop]
-        else:
-            instance = super(AsyncHTTPClient, cls).__new__(impl)
-            args = {}
-            if cls._impl_kwargs:
-                args.update(cls._impl_kwargs)
-            args.update(kwargs)
-            instance.initialize(io_loop, max_clients, **args)
-            if not force_instance:
-                impl._async_clients()[io_loop] = instance
-            return instance
-
-    def close(self):
-        """Destroys this http client, freeing any file descriptors used.
-        Not needed in normal use, but may be helpful in unittests that
-        create and destroy http clients.  No other methods may be called
-        on the AsyncHTTPClient after close().
-        """
-        if self._async_clients().get(self.io_loop) is self:
-            del self._async_clients()[self.io_loop]
-
+    _rpcs = []
+    
+    """An non-blocking HTTP client that uses `google.appengine.api.urlfetch`."""
     def fetch(self, request, callback, **kwargs):
-        """Executes a request, calling callback with an `HTTPResponse`.
+        if not isinstance(request, HTTPRequest):
+            request = HTTPRequest(url=request, **kwargs)
 
-        The request may be either a string URL or an `HTTPRequest` object.
-        If it is a string, we construct an `HTTPRequest` using any additional
-        kwargs: ``HTTPRequest(request, **kwargs)``
+        url = request.url
+        kwargs = {
+            'payload': request.body,
+            'method': request.method,
+            'headers': request.headers,
+            'allow_truncated': False,
+            'follow_redirects': request.follow_redirects,
+            'validate_certificate': request.validate_cert
+            }
 
-        If an error occurs during the fetch, the HTTPResponse given to the
-        callback has a non-None error attribute that contains the exception
-        encountered during the request. You can call response.rethrow() to
-        throw the exception (if any) in the callback.
-        """
-        raise NotImplementedError()
+        rpc = urlfetch.create_rpc()
+        rpc.callback = create_rpc_callback(rpc, callback, request=request)
+        urlfetch.make_fetch_call(rpc, url, **kwargs)
+        self._rpcs.append(rpc)
 
-    @staticmethod
-    def configure(impl, **kwargs):
-        """Configures the AsyncHTTPClient subclass to use.
+    def wait_all(self):
+        for rpc in self._rpcs:
+            rpc.wait()
 
-        AsyncHTTPClient() actually creates an instance of a subclass.
-        This method may be called with either a class object or the
-        fully-qualified name of such a class (or None to use the default,
-        SimpleAsyncHTTPClient)
 
-        If additional keyword arguments are given, they will be passed
-        to the constructor of each subclass instance created.  The
-        keyword argument max_clients determines the maximum number of
-        simultaneous fetch() operations that can execute in parallel
-        on each IOLoop.  Additional arguments may be supported depending
-        on the implementation class in use.
+def create_rpc_callback(rpc, callback, *args, **kwargs):
+    """Returns a wrapped callback for an async request."""
+    if callback is None:
+        return None
 
-        Example::
+    request = kwargs.pop('request')
 
-           AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
-        """
-        if isinstance(impl, (unicode, bytes_type)):
-            impl = import_object(impl)
-        if impl is not None and not issubclass(impl, AsyncHTTPClient):
-            raise ValueError("Invalid AsyncHTTPClient implementation")
-        AsyncHTTPClient._impl_class = impl
-        AsyncHTTPClient._impl_kwargs = kwargs
+    if args or kwargs:
+        callback = functools.partial(callback, *args, **kwargs)
+    
+    def wrapper(*args, **kwargs):
+        try:
+            result = rpc.get_result()
+            response = HTTPResponse(
+                request=request,
+                code=result.status_code,
+                headers=result.headers,
+                body=result.content,
+                effective_url=result.final_url)
+        except urlfetch.DownloadError, e:
+            response = HTTPResponse(request, 599,
+                                    error=HTTPError(599, e.message))
+            
+        try:
+            args += (response,)
+            return callback(*args, **kwargs)
+        except Exception, e:
+            logging.error("Exception during callback", exc_info=True)
+
+    return wrapper
+
 
 class HTTPRequest(object):
     """HTTP client request object."""
@@ -293,6 +174,8 @@ class HTTPRequest(object):
         self.client_cert = client_cert
         self.start_time = time.time()
 
+    def __str__(self):
+        return "%s %s" % (self.method, self.url)
 
 class HTTPResponse(object):
     """HTTP Response object.
@@ -319,14 +202,14 @@ class HTTPResponse(object):
         plus 'queue', which is the delay (if any) introduced by waiting for
         a slot under AsyncHTTPClient's max_clients setting.
     """
-    def __init__(self, request, code, headers={}, buffer=None,
+    def __init__(self, request, code, headers={}, buffer=None, body=None,
                  effective_url=None, error=None, request_time=None,
                  time_info={}):
         self.request = request
         self.code = code
         self.headers = headers
         self.buffer = buffer
-        self._body = None
+        self.body = body
         if effective_url is None:
             self.effective_url = request.url
         else:
@@ -340,16 +223,6 @@ class HTTPResponse(object):
             self.error = error
         self.request_time = request_time
         self.time_info = time_info
-
-    def _get_body(self):
-        if self.buffer is None:
-            return None
-        elif self._body is None:
-            self._body = self.buffer.getvalue()
-
-        return self._body
-
-    body = property(_get_body)
 
     def rethrow(self):
         """If there was an error on the request, raise an `HTTPError`."""
@@ -380,29 +253,3 @@ class HTTPError(Exception):
         message = message or httplib.responses.get(code, "Unknown")
         self.response = response
         Exception.__init__(self, "HTTP %d: %s" % (self.code, message))
-
-
-def main():
-    from tornado.options import define, options, parse_command_line
-    define("print_headers", type=bool, default=False)
-    define("print_body", type=bool, default=True)
-    define("follow_redirects", type=bool, default=True)
-    args = parse_command_line()
-    client = HTTPClient()
-    for arg in args:
-        try:
-            response = client.fetch(arg,
-                                    follow_redirects=options.follow_redirects)
-        except HTTPError, e:
-            if e.response is not None:
-                response = e.response
-            else:
-                raise
-        if options.print_headers:
-            print response.headers
-        if options.print_body:
-            print response.body
-    client.close()
-
-if __name__ == "__main__":
-    main()
